dde5fe42772ac7f35921286fad3ac5af519298e9
Nery José Barrientos Posadas <3386309102008@ingenieria.usac.edu.gt>
2024-12-04 16:57:18 -0600

    este si xd

--- ProyectoVacas/vite.config.ts
+++ ProyectoVacas/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'prueba'
+  base: '/prueba/' // Asegúrate de que coincida con el nombre de tu repositorio
 })
