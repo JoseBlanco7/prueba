323f2815b997c4b59c184224e016f49b58881fe8
joseblanco7 <cjbg9999@gmail.com>
2024-12-04 18:30:03 -0600

    deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/prueba/' // Asegúrate de que coincida con el nombre de tu repositorio
-})
+});
